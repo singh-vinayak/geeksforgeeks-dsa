76cef3d76520710393c9f664a539a895897f3025
Vinayak Singh <vinayaksingh5302@gmail.com>
2020-12-04 14:10:05 +0530

    sorted

---
+++ Arrays/isSorted.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class isSorted
+{ 
+    static boolean isSorted(int arr[], int n)
+    {
+    	for(int i = 1; i < n; i++)
+    	{
+    	    if(arr[i] < arr[i - 1])
+    	        return false;
+    	}
+
+    	return true;
+    } 
+
+    public static void main(String args[]) 
+    { 
+       int arr[] = {5, 12, 30, 2, 35}, n = 5;
+
+       System.out.println(isSorted(arr, n));
+    } 
+}
