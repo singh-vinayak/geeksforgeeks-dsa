4ef8b0ca29c60d2c67c47e09cdbee39935e1dc8f
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-02-03 12:25:48 +0530

    zero sum subarray

---
+++ Hashing/subarray0sum.java
@@ -0,0 +1,29 @@
+//find a subarray with sum equal to zero
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class subarray0sum{
+    public static void main(String[] args) {
+        int arr[] = new int[] { 5, 3, 9, -4, -6, 7, -1 };
+        int n = arr.length;
+
+        System.out.println(ZeroSumSubarray(arr, n));
+
+    }
+
+    static int ZeroSumSubarray(int arr[], int n) {
+        Set<Integer> us = new HashSet<Integer>();
+        int prefix_sum = 0;
+        us.add(0);
+        for (int i = 0; i < n; i++) {
+            prefix_sum += arr[i];
+            if (us.contains(prefix_sum) == true)
+                return 1;
+
+            us.add(prefix_sum);
+        }
+
+        return 0;
+    }
+}
